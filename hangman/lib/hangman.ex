7aed37e4ddbc2d63a9806cb95663583efafa22dc
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-25 17:58:12 +0200

    feat(api): add public functions to api

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,6 +1,12 @@
 defmodule Hangman do
   alias Hangman.Game
 
-  defdelegate init_game, to: Game, as: :new_game
+  defdelegate init_game(),  to: Game, as: :new_game
+  defdelegate tally(game),  to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    { game, tally(game) }
+  end
 
 end
