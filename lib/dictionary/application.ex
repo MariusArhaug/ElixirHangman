c2c4f68f588953a7e549232d1e413af6cf78140a
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-26 22:34:02 +0200

    feat(supervisor): add supervisor to API

--- lib/dictionary/application.ex
+++ lib/dictionary/application.ex
@@ -2,6 +2,17 @@ defmodule Dictionary.Application do
   use Application
 
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    import Supervisor.Spec, warn: false
+
+    children = [
+      %{id: Dictionary.WordList, start: {Dictionary.WordList, :start_link, []}}
+    ]
+
+    options = [
+      name: Dictionary.Superviser,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
