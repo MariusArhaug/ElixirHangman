37e54f1c5d9b3b8c0e9634e3dca6a4021b1fd7b9
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-27 22:26:55 +0200

    refactor(API): delegate method to game.ex

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -4,9 +4,6 @@ defmodule Hangman do
   defdelegate init_game(),  to: Game, as: :new_game
   defdelegate tally(game),  to: Game
 
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    { game, tally(game) }
-  end
+  defdelegate make_move(game, guess), to: Game
 
 end
