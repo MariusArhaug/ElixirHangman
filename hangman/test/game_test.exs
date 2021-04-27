b61fb83a2e000869566343462aab21ca0708b3ed
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-27 22:27:29 +0200

    refactor(tests): update return types for tests

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -21,28 +21,28 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won , :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert ^game = Game.make_move(game, "x")
+      assert { ^game, _tally } = Game.make_move(game, "x")
     end
   end
 
   test "first occurence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    { game, _tally } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
@@ -59,7 +59,7 @@ defmodule GameTest do
     game = Game.new_game("wibble")
 
     Enum.reduce(moves, game, fn ({guess, state}, new_game) ->
-      new_game = Game.make_move(new_game, guess)
+      { new_game, _tally } = Game.make_move(new_game, guess)
       assert new_game.game_state == state
       new_game
     end)
@@ -67,7 +67,7 @@ defmodule GameTest do
 
   test "bad guess is recognized" do
     game = Game.new_game("wibble")
-    game= Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
@@ -86,7 +86,7 @@ defmodule GameTest do
     game = Game.new_game("wibble")
 
     Enum.reduce(moves, game, fn ({guess, state, turns_left} , game) ->
-      game = Game.make_move(game, guess)
+      { game, _tally } = Game.make_move(game, guess)
       assert game.game_state == state
       assert game.turns_left == turns_left
       game
