5a07318b7af7405f786271fbf58ea02ab1388ef5
MariusArhaug <mariusarhaug@hotmail.com>
2021-05-19 00:40:11 +0200

    wip(ux): add ux and view logic

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,4 +1,35 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
   def join_letters(letters), do: letters |> Enum.join(" ")
+
+  def game_over?(%{ game_state: game_state}) do
+    game_state in [ :won, :lost ]
+  end
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  @responses %{
+    :won  => { :success, "You won!"},
+    :lost => { :danger, "You lost!"},
+    :good_guess => { :success, "Good guess!"},
+    :bad_guess  => { :warning, "Bad guess!" },
+    :already_used => { :info, "You already guessed that!"}
+  }
+
+  def game_state(state) do
+    @responses[state]
+    |> alert
+  end
+
+  defp alert(nil), do: ""
+
+  defp alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
 end
