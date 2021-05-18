7c69a9968fee1d8c88ce4a7f22157ce573b3e78c
MariusArhaug <mariusarhaug@hotmail.com>
2021-05-19 00:46:59 +0200

    refactor(hangman_view): move state function into helper file

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,5 +1,8 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  import Gallows.Views.Helpers.GameStateHelper
+
   def join_letters(letters), do: letters |> Enum.join(" ")
 
   def game_over?(%{ game_state: game_state}) do
@@ -9,27 +12,4 @@ defmodule GallowsWeb.HangmanView do
     button("New Game", to: Routes.hangman_path(conn, :create_game))
   end
 
-  @responses %{
-    :won  => { :success, "You won!"},
-    :lost => { :danger, "You lost!"},
-    :good_guess => { :success, "Good guess!"},
-    :bad_guess  => { :warning, "Bad guess!" },
-    :already_used => { :info, "You already guessed that!"}
-  }
-
-  def game_state(state) do
-    @responses[state]
-    |> alert
-  end
-
-  defp alert(nil), do: ""
-
-  defp alert({class, message}) do
-    """
-    <div class="alert alert-#{class}">
-      #{message}
-    </div>
-    """
-    |> raw()
-  end
 end
