63f9683cbab9bc4f106b03d1f80d7e133d5127c1
MariusArhaug <mariusarhaug@hotmail.com>
2021-05-19 20:07:31 +0200

    feat(opacity): add style opacity to svg

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -3,6 +3,14 @@ defmodule GallowsWeb.HangmanView do
 
   import Gallows.Views.Helpers.GameStateHelper
 
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
+
   def join_letters(letters), do: letters |> Enum.join(" ")
 
   def game_over?(%{ game_state: game_state}) do
